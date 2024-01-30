c078515460db6f284300060355dfb3d58f6f566f
trangdangberlin <dang38073@gmail.com>
2024-01-30 22:06:02 +0100

    change image domain

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,11 +14,7 @@
     <Cheatsheet />
     <Tables />
     <img src="trinity_dodge_this.jpeg" alt="Dodge this" />
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Digital_rain_animation_medium_letters_shine.gif"
-      alt="Original Image"
-      class="mb-4"
-    />
+    <img src="digital_rain.gif" alt="digital_rain" />
   </div>
 </div>
 
