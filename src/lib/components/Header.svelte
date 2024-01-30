290370efe718ac045e3553bccdb57f03e486187c
trangdangberlin <dang38073@gmail.com>
2024-01-30 21:41:32 +0100

    components

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+  function changeColor() {
+    const colorOptions = ["#0000FF", "#FF0000"];
+    const randomColor =
+      colorOptions[Math.floor(Math.random() * colorOptions.length)];
+    console.log(randomColor);
+
+    document.body.style.backgroundColor = randomColor;
+  }
+</script>
+
+<div id="header" class="text-center space-y-2 pt-10 font-bold ju">
+  <h1 class="text-4xl text-green-500">Trang Coding Diary</h1>
+  <div>
+    <button
+      class="bg-green-500 rounded hover:bg-green-700 px-2 text-neutral-50 text-xl"
+    >
+      <a href="./index.html"> Home </a>
+    </button>
+    <button
+      class="bg-green-500 rounded hover:bg-green-700 px-2 text-neutral-50 text-xl"
+    >
+      <a href="./aboutme.html" target="_blank"> About me </a>
+    </button>
+    <button
+      class="bg-green-500 rounded hover:bg-green-700 px-2 text-neutral-50 text-xl"
+      on:click={changeColor}
+    >
+      Change Color
+    </button>
+  </div>
+</div>
---
+++ src/lib/components/Tables.svelte
@@ -0,0 +1,46 @@
+<table class="table-auto mb-4">
+  <thead>
+    <tr>
+      <th class="border p-2">heading 1</th>
+      <th class="border p-2">heading 2</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border p-2">col 1</td>
+      <td class="border p-2">col 2</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table-auto mb-4">
+  <tr>
+    <th class="border p-2">column one</th>
+    <th class="border p-2">column two</th>
+  </tr>
+  <tr>
+    <td class="border p-2">1</td>
+    <td class="border p-2">2</td>
+  </tr>
+</table>
+
+<table class="table-auto mb-4">
+  <tr>
+    <td class="border p-2"> Table row element </td>
+  </tr>
+</table>
+
+<table class="table-auto mb-4">
+  <tr>
+    <th class="border p-2">row 1:</th>
+    <td class="border p-2">col 1</td>
+    <td class="border p-2">col 2</td>
+    <td class="border p-2">col 3</td>
+  </tr>
+  <tr>
+    <th class="border p-2">row 2:</th>
+    <td class="border p-2" colspan="2">col 1 (will span 2 columns)</td>
+    <td class="border p-2">col 2</td>
+    <td class="border p-2">col 3</td>
+  </tr>
+</table>
