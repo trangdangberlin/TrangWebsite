d935b92a084a1545c5fa592aaa4ad91002ad621b
trangdangberlin <dang38073@gmail.com>
2024-01-31 22:48:06 +0100

    change

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,7 +36,6 @@
       on:click={changeColor}
     >
       Change Color
-    </button>
-    </body>
+
   </div>
 </div>
