fc435031e561b319e4b64b2f7904942b9357e84c
trangdangberlin <dang38073@gmail.com>
2024-02-02 21:20:23 +0100

    change footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,20 @@
 <script>
 </script>
 
-<div class="bg-zinc-950 text-neutral-100 mt-24">
+<div class="bg-green-600 text-neutral-100 mt-24 flex justify-between">
   <div class="text-center space-y-2 pt-10 font-bold">
-    <h4 class="text-4xl text-green-500">Goodbye</h4>
+    <h4 class="text-4xl text-zinc-950">Goodbye</h4>
   </div>
   <div class="mt-10 ml-10">
     <div>Trang D.</div>
     <div>Data Analytics</div>
     <div>Berlin</div>
+  </div>
+
+  <div>
+    <div><img class="w-5" src="Li_icon.png"></div>
+    <div><img class="w-5" src="GitHub_icon.png"></div>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <div><img class="w-5" src="email_icon.png"></div>
   </div>
 </div>
