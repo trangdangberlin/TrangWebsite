98ab3fc86520ff0fe0c027c534dd7baf1f3e2deb
trangdangberlin <dang38073@gmail.com>
2024-02-05 00:00:18 +0800

    change text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,8 +5,12 @@
   src="trinity_dodge_this.jpeg"
   alt="Dodge this"
   class="block mx-auto my-4 rounded-lg"
-  Hi there! I'm Trang a sales expert and entry level data professional with over five years of experience in sales and customer management. My passion for bridging cultures and technologies stems from growing up in an exciting environment that blends Berlin and my Vietnamese heritage.
-
-For me, "Achieving more and embracing better challenges" is not just a saying; it's a guiding principle in both life and work.
-
 />
+<div class="text-neutral-100 text-center">
+  Hi there! I'm Trang a sales expert and entry level data professional with over
+  five years of experience in sales and customer management. My passion for
+  bridging cultures and technologies stems from growing up in an exciting
+  environment that blends Berlin and my Vietnamese heritage. For me, "Achieving
+  more and embracing better challenges" is not just a saying; it's a guiding
+  principle in both life and work.
+</div>
