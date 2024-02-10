2d4a865a57535525526623a49429fc9b5f59cb7d
trangdangberlin <dang38073@gmail.com>
2024-02-10 21:43:57 +0700

    change text white

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -27,18 +27,18 @@
       class="bg-zinc-950 flex items-center justify-center h-screen relative text-white"
     >
       <div
-        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold "
+        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl text-white font-semibold"
       >
         Click 1
       </div>
       <button
-        class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0 "
+        class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0"
       >
         <a href="/">Click 1</a>
       </button>
 
       <div
-        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold"
+        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl text-white font-semibold"
       >
         Click 2
       </div>
@@ -49,7 +49,7 @@
       </button>
 
       <div
-        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold"
+        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl text-white font-semibold"
       >
         Click 3
       </div>
