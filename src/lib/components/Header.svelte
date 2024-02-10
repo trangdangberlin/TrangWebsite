bfbf466b5559601ccdeed867bc4838813b32079c
trangdangberlin <dang38073@gmail.com>
2024-02-10 21:31:49 +0700

    change

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -23,14 +23,16 @@
     </style>
   </head>
   <body>
-    <div class="bg-gray-100 flex items-center justify-center h-screen relative">
+    <div
+      class="bg-zinc-950 flex items-center justify-center h-screen relative text-white"
+    >
       <div
-        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold"
+        class="text-zinc-950 transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold "
       >
         Click 1
       </div>
       <button
-        class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0"
+        class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0 "
       >
         <a href="/">Click 1</a>
       </button>
