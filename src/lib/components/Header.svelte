215d154aa1fbf97503f63fdf4b1b7351c6d5f651
trangdangberlin <dang38073@gmail.com>
2024-02-11 22:02:22 +0700

    change header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,33 +1,30 @@
-<div class="flex justify-around bg-black text-white py-12 ">
-  <div class="font-bold text-2xl">
-      TRANG WEB
-  </div>
-  <div class="font-bold text-2xl">
-      Trang Web
-  </div>
+<div class="flex justify-around bg-black text-white py-12">
+  <div class="font-bold text-2xl">TRANG WEB</div>
+
   <div class="bg-zinc-950 flex items-center justify-center text-white">
-    <div class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold">
+    <div
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+    >
       Click 1
     </div>
-    <button class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0" onclick="toggleTextColor('parentDiv')">
-      <a href="/">Click 1</a>
-    </button>
 
-    <div class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold">
+    <div
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+    >
       Click 2
     </div>
-    <button class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0" onclick="toggleTextColor('parentDiv')">
-      <a href="/">Click 2</a>
-    </button>
 
-    <div class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold">
+    <div
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+    >
       Click 3
     </div>
-    <button class="hidden absolute top-0 left-0 bg-green-500 text-white px-4 py-2 transition-all duration-300 ease-in-out transform translate-x-full group-hover:translate-x-0" onclick="toggleTextColor('parentDiv')">
-      <a href="/">Click 3</a>
-    </button>
   </div>
-  <div class="font-bold text-2xl">
+  <button class=" border-white bg-black hover:bg-green-500 rounded-xl px-2 hover:text-black">
+    <div
+      class="font-bold text-2xl text-white transition-colors duration-300 ease-in-out"
+    >
       Contact
-  </div>
+    </div>
+  </button>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     fontFamily: {
-      sansserif: ['Roboto']
+      sans: ['Roboto']
     },
     extend: {},
   },
