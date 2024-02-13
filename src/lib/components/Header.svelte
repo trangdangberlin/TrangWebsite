2d60f9fadcd6a0468134a927e76f2ef1cf1b6e6f
trangdangberlin <dang38073@gmail.com>
2024-02-13 19:44:49 +0700

    change style font size

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,29 +2,36 @@
   <div class="font-bold text-2xl">TRANG WEB</div>
 
   <div class="bg-black flex items-center justify-center text-white">
+
+     <div class="button-group flex space-x-10">
+    
+  
     <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
     >
       CLICK 1
     </div>
 
     <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
     >
       CLICK 2
     </div>
 
     <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl font-semibold hover:text-black"
+      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
     >
       CLICK 3
     </div>
   </div>
+  </div>
+
   <button class="bg-black hover:bg-green-500 rounded-xl px-2 hover:text-black">
     <div
-      class="font-bold text-2xl text-white transition-colors duration-300 ease-in-out"
+      class="text-xl text-white transition-colors duration-300 ease-in-out"
     >
       CONTACT
     </div>
+     
   </button>
 </div>
