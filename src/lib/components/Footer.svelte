8ef91c37bf56692e72ec275405de776d5aa3145a
trangdangberlin <dang38073@gmail.com>
2024-02-13 15:52:46 +0700

    change footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,11 +2,11 @@
 </script>
 
 <div
-  class="bg-green-600 text-black font-semibold mt-24 flex justify-between items-center align-middle h-24"
+  class="bg-white text-black font-semibold flex justify-between items-center align-middle h-24"
 >
   <div class="ml-10">Trang D. - Berlin</div>
 
-  <div class="flex flex-row space-x-1 mr-10">
+  <div class="flex flex-row space-x-1 mr-10 ">
     <a href="https://www.linkedin.com/in/dang-trang/" target="_blank">
       <img class="w-5 h-5" src="Li_icon.png" />
     </a>
@@ -16,7 +16,6 @@
     </a>
 
     <a href="mailto:da.trang91@yahoo.de" target="_blank">
-
       <img class="w-5 h-5" src="email_icon.png" />
     </a>
   </div>
