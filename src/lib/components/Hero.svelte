7adf267faf34b14d50c856080d8e4aa01b2dc3b2
trangdangberlin <dang38073@gmail.com>
2024-02-13 15:53:40 +0700

    change text

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,15 +1,33 @@
 <div class="relative bg-black text-white">
   <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-    <div class="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
+    <div
+      class="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6"
+    >
       <div class="mx-auto max-w-2xl lg:mx-0">
-        <div class="hidden sm:mt-32 sm:flex lg:mt-16">
-        </div>
-        <h1 class="mt-24 text-4xl font-bold tracking-tight  sm:mt-10 sm:text-6xl">Welcome to my website</h1>
-        <p class="mt-6 text-lg leading-8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+        <div class="hidden sm:mt-32 sm:flex lg:mt-16"></div>
+        <h1
+          class="mt-24 text-4xl font-bold tracking-tight sm:mt-10 sm:text-6xl"
+        >
+          Welcome to my website
+        </h1>
+        <p class="mt-6 text-lg leading-8">
+          Hi there! I'm Trang a sales expert and entry level data professional
+          with over five years of experience in sales and customer management.
+          My passion for bridging cultures and technologies stems from growing
+          up in an exciting environment that blends Berlin and my Vietnamese
+          heritage. For me, "Achieving more and embracing better challenges" is
+          not just a saying; it's a guiding principle in both life and work.
+        </p>
       </div>
     </div>
-    <div class="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
-      <img class="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full" src="https://scontent-sin6-4.xx.fbcdn.net/v/t39.30808-6/417542805_1075576213770710_3184805593950548391_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHj_0a_fxjQU0B6z6_BWyGTtVReRmD0KGC1VF5GYPQoYEluFlEKhTuf4igyCWdIO9k&_nc_ohc=920ozKWvGG8AX_FG7Um&_nc_ht=scontent-sin6-4.xx&oh=00_AfC2Fr133VeESNHv8ZsCjkv6RtLSVrV-9VVY1h3sEmHwsQ&oe=65CE87A1" alt="">
+    <div
+      class="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0"
+    >
+      <img
+        class="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+        src="https://scontent-sin6-4.xx.fbcdn.net/v/t39.30808-6/417542805_1075576213770710_3184805593950548391_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHj_0a_fxjQU0B6z6_BWyGTtVReRmD0KGC1VF5GYPQoYEluFlEKhTuf4igyCWdIO9k&_nc_ohc=920ozKWvGG8AX_FG7Um&_nc_ht=scontent-sin6-4.xx&oh=00_AfC2Fr133VeESNHv8ZsCjkv6RtLSVrV-9VVY1h3sEmHwsQ&oe=65CE87A1"
+        alt=""
+      />
     </div>
   </div>
 </div>
