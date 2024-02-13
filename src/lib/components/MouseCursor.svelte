f9c26ec2a1cead64cb98d11dd4bea3c501907812
trangdangberlin <dang38073@gmail.com>
2024-02-13 16:13:26 +0700

    change curser

---
+++ src/lib/components/MouseCursor.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let dotX = 0;
+  let dotY = 0;
+  let showCursor = false;
+
+  onMount(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  });
+
+  function handleMouseMove(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+
+    // Introduce a delay to the dot's position update
+    dotX = mouseX;
+    dotY = mouseY;
+
+   
+      showCursor = true;
+  }
+</script>
+
+<style>
+  .cursor {
+    position: fixed;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: greenyellow; /* Change to your desired color */
+    pointer-events: none;
+    transform: translate(-50%, -50%);
+    transition: transform 0.1s ease-out;
+    z-index: 9999; /* Adjust z-index to be higher than other elements */
+  }
+</style>
+
+{#if showCursor}
+  <div class="cursor" style={`left: ${dotX}px; top: ${dotY}px;`}></div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import MouseCursor from "$lib/components/MouseCursor.svelte";
 </script>
+
+<MouseCursor />
 
 <Header />
 <slot />
