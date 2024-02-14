4883713e5c7e511f5e9d6f7cff764bf5b47dfa49
trangdangberlin <dang38073@gmail.com>
2024-02-14 16:22:41 +0700

    globlas css style

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,37 +1,38 @@
-<div class="flex justify-around bg-black text-white py-12">
-  <div class="font-bold text-2xl">TRANG WEB</div>
+<div class="flex justify-around bg-black py-12">
+  <a href="/">
+    <div class="font-bold text-2xl text-white">TRANG WEB</div>
+  </a>
 
   <div class="bg-black flex items-center justify-center text-white">
+    <div class="button-group flex space-x-10">
+      <a href="/about">
+        <div
+          class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
+        >
+          ABOUT ME
+        </div>
+      </a>
+      <a href="/portfolio">
+        <div
+          class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
+        >
+          PORTFOLIO
+        </div>
+      </a>
 
-     <div class="button-group flex space-x-10">
-    
-  
-    <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
-    >
-      CLICK 1
-    </div>
-
-    <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
-    >
-      CLICK 2
-    </div>
-
-    <div
-      class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
-    >
-      CLICK 3
+      <div
+        class="text-white transition-colors duration-300 ease-in-out hover:bg-green-500 rounded-xl px-2 text-xl hover:text-black"
+      >
+        CLICK 3
+      </div>
     </div>
   </div>
-  </div>
 
-  <button class="bg-black hover:bg-green-500 rounded-xl px-2 hover:text-black">
+  <button class="bg-black rounded-xl px-2 hover:bg-green-500">
     <div
-      class="text-xl text-white transition-colors duration-300 ease-in-out"
+      class="text-white text-xl transition-colors duration-300 ease-in-out hover:text-black"
     >
       CONTACT
     </div>
-     
   </button>
 </div>
