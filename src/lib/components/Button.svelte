535f004405678383614a49cf3c808729adfeb28f
trangdangberlin <dang38073@gmail.com>
2024-02-17 06:54:14 +0700

    change button group, mobile version

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,19 +1,23 @@
 <script>
 export let title;
+export let link;
 </script>
 
-<button class="btnAnimation text-xl bg-black text-white border-0 rounded-xl px-2 relative font-semibold tracking-wider leading-none overflow-hidden hover:text-black" type="button">
+
+<a href="{link}">
+<button class="btnAnimation text-xl bg-black text-white border-0 rounded-2xl px-3 py-2 relative tracking-wider leading-none overflow-hidden hover:text-black" type="button">
     <span class="absolute inset-0 bg-neongreen"></span>
-    <span class="absolute inset-0 flex justify-center items-center font-bold"> 
+    <span class="absolute inset-0 flex justify-center items-center"> 
     {title}
     </span>
     {title}
 </button>
+</a>
 
 <style>
 .btnAnimation span:first-child{
   transform: translateY(101%);
-  transition: transform .3s ease-in
+  transition: transform .2s ease-in
 }
 .btnAnimation:hover span{
   transform: translateX(0)
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,42 +2,11 @@
   import Button from "$lib/components/Button.svelte";
  
 </script>
-<div class="flex justify-around bg-black py-12">
+<div class="flex flex-col sm:flex-row justify-around bg-black py-12">
   <a href="/">
     <div class="font-bold text-2xl text-white">TRANG WEB</div>
   </a>
-
-  <div class="bg-black flex items-center justify-center text-white">
-    <div class="button-group flex space-x-10">
-      <Button title="ABOUT ME"/>
-      <a href="/about">
-        <div
-          class="text-white transition-colors duration-300 ease-in-out hover:bg-neongreen rounded-xl px-2 text-xl hover:text-black"
-        >
-          ABOUT ME
-        </div>
-      </a>
-      <a href="/portfolio">
-        <div
-          class="text-white transition-colors duration-300 ease-in-out hover:bg-neongreen rounded-xl px-2 text-xl hover:text-black"
-        >
-          PORTFOLIO
-        </div>
-      </a>
-
-      <div
-        class="text-white transition-colors duration-300 ease-in-out hover:bg-neongreen rounded-xl px-2 text-xl hover:text-black"
-      >
-        CLICK 3
-      </div>
-    </div>
-  </div>
-
-  <button class="bg-black rounded-xl px-2 hover:bg-neongreen">
-    <div
-      class="text-white text-xl transition-colors duration-300 ease-in-out hover:text-black"
-    >
-      CONTACT
-    </div>
-  </button>
+  <Button title="ABOUT ME" link="/about"/>
+  <Button title="PORTFOLIO" link="/portfolio"/> 
+  <Button title="CONTACT" link="/contact"/>
 </div>
