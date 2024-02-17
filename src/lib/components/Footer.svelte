18490990d18e8ae458b26a693c2a48173847ad3d
trangdangberlin <dang38073@gmail.com>
2024-02-17 22:38:13 +0700

    change img

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,21 +2,21 @@
 </script>
 
 <div
-  class="bg-white text-black font-semibold flex justify-between items-center align-middle h-24"
+  class="bg-black text-white font-semibold flex justify-between items-center align-middle h-24"
 >
   <div class="ml-10">Trang D. - Berlin</div>
 
-  <div class="flex flex-row space-x-1 mr-10 ">
+  <div class="flex flex-row space-x-2 mr-10">
     <a href="https://www.linkedin.com/in/dang-trang/" target="_blank">
-      <img class="w-5 h-5" src="Li_icon.png" />
+      <img class="w-5 h-5" src="LinkedIn_logo.png" />
     </a>
 
     <a href="https://github.com/trangdangberlin" target="_blank">
-      <img class="w-5 h-5" src="GitHub_icon.png" />
+      <img class="w-5 h-5" src="Github.png" />
     </a>
-    
+
     <a href="mailto:da.trang91@yahoo.de" target="_blank">
-      <img class="w-5 h-5" src="email_icon.png" />
+      <img class="w-5 h-5" src="email.png" />
     </a>
   </div>
 </div>
