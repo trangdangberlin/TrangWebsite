a468dd8ed59e90b468d3df422b4c0e4cfa68a48d
trangdangberlin <dang38073@gmail.com>
2024-02-19 22:42:25 +0700

    change img

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,26 +1,43 @@
 <script>
 </script>
 
-<div class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white overflow-x-scroll rounded-box">
+<div
+  class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white overflow-x-scroll rounded-box"
+>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" class="rounded-box" />
-  </div> 
+    <img src="trinity_dodge_this.jpeg" class="rounded-box w-96 h-fit" />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" class="rounded-box" />
-  </div> 
+    <img src="Trang.jpeg" class="rounded-box  w-96 h-fit" />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" class="rounded-box" />
-  </div> 
+    <img
+      src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+      class="rounded-box  w-96 h-fit"
+    />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" class="rounded-box" />
-  </div> 
+    <img
+      src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+      class="rounded-box"
+    />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" class="rounded-box" />
-  </div> 
+    <img
+      src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+      class="rounded-box"
+    />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" class="rounded-box" />
-  </div> 
+    <img
+      src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+      class="rounded-box"
+    />
+  </div>
   <div class="carousel-item">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+    <img
+      src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+      class="rounded-box"
+    />
   </div>
 </div>
