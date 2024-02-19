91817a74a1d6955b6b9da605372d1827efdfa398
trangdangberlin <dang38073@gmail.com>
2024-02-19 22:43:59 +0700

    change img

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -19,25 +19,25 @@
   <div class="carousel-item">
     <img
       src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-      class="rounded-box"
+      class="rounded-box w-96 h-fit"
     />
   </div>
   <div class="carousel-item">
     <img
       src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-      class="rounded-box"
+      class="rounded-box w-96 h-fit"
     />
   </div>
   <div class="carousel-item">
     <img
       src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-      class="rounded-box"
+      class="rounded-box w-96 h-fit"
     />
   </div>
   <div class="carousel-item">
     <img
       src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-      class="rounded-box"
+      class="rounded-box w-96 h-fit"
     />
   </div>
 </div>
