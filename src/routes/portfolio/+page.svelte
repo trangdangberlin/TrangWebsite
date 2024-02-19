c41aee63c3b33b4a783b35754cde157fb8933fce
trangdangberlin <dang38073@gmail.com>
2024-02-19 22:51:12 +0700

    change img

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -5,39 +5,24 @@
   class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white overflow-x-scroll rounded-box"
 >
   <div class="carousel-item">
-    <img src="trinity_dodge_this.jpeg" class="rounded-box w-96 h-fit" />
+    <img src="trinity.png" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img src="Trang.jpeg" class="rounded-box  w-96 h-fit" />
+    <img src="Trang.jpeg" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img
-      src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-      class="rounded-box  w-96 h-fit"
-    />
+    <img src="mininal_bathroom.jpeg" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img
-      src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-      class="rounded-box w-96 h-fit"
-    />
+    <img src="pingpong.jpeg" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img
-      src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-      class="rounded-box w-96 h-fit"
-    />
+    <img src="chess_wood.jpeg" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img
-      src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-      class="rounded-box w-96 h-fit"
-    />
+    <img src="building.webp" class="rounded-box w-96 h-fit" />
   </div>
   <div class="carousel-item">
-    <img
-      src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-      class="rounded-box w-96 h-fit"
-    />
+    <img src="neo.png" class="rounded-box w-96 h-fit" />
   </div>
 </div>
