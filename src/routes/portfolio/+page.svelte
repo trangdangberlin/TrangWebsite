1555809e56dcf497192d8df07c5703dc57a33e54
trangdangberlin <dang38073@gmail.com>
2024-02-21 16:26:46 +0700

    change img

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,20 +1,19 @@
 <script>
 </script>
 
-<div
-  class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white"
->
-   <a href="your_link_here" class="block w-full h-full">
-      <img src="Trang.jpeg" class="rounded-box w-full h-full object-cover" />
-      <p class="text-center mt-2">Your Text Here</p>
+<div class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white">
   <div class="carousel-item w-96 h-96">
     <img src="Trang.jpeg" class="rounded-box w-full h-full object-cover" />
   </div>
+  <p class="text-center mt-2">Your Text Here</p>
   <div class="carousel-item w-96 h-96">
     <img src="matrix.png" class="rounded-box w-full h-full object-cover" />
   </div>
   <div class="carousel-item w-96 h-96">
-    <img src="mininal_bathroom.jpeg" class="rounded-box w-full h-full object-cover" />
+    <img
+      src="mininal_bathroom.jpeg"
+      class="rounded-box w-full h-full object-cover"
+    />
   </div>
   <div class="carousel-item w-96 h-96">
     <img src="pingpong.jpeg" class="rounded-box w-full h-full object-cover" />
