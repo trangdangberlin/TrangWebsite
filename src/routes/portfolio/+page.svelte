07387bb7f0e2f181bd20e1b4b6d3d21920f0cd4b
trangdangberlin <dang38073@gmail.com>
2024-02-21 16:49:58 +0700

    change link

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -2,10 +2,11 @@
 </script>
 
 <div class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white">
-  <div class="carousel-item w-96 h-96">
+
+ <a href="your_link_here" class="carousel-item w-96 h-96 relative block">
     <img src="Trang.jpeg" class="rounded-box w-full h-full object-cover" />
-  </div>
-  <p class="text-center mt-2">Your Text Here</p>
+    <div class="text-center mt-2 ml-2 absolute bottom-2 left-0 font-semibold text-white">Your Text Here</div>
+</a>
   <div class="carousel-item w-96 h-96">
     <img src="trinity.png" class="rounded-box w-full h-full object-cover" />
   </div>
