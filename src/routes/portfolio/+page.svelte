78ce3a64eafd80db4298e23987fd118e3561d395
trangdangberlin <dang38073@gmail.com>
2024-02-23 22:09:50 +0100

    change

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -2,18 +2,22 @@
 </script>
 
 <div class="carousel carousel-center w-full bg-black p-4 space-x-4 text-white">
-
- <a href="/portfolio/project_one" class="carousel-item w-96 h-96 relative block">
+  <a
+    href="/portfolio/project_one"
+    class="carousel-item w-96 h-96 relative block"
+  >
     <img src="Trang.jpeg" class="rounded-box w-full h-full object-cover" />
-    <div class="text-center mt-2 ml-2 absolute bottom-2 left-0 font-semibold text-white">Your Text Here</div>
-</a>
+    <div
+      class="text-center mt-2 ml-2 absolute bottom-2 left-0 font-semibold text-white"
+    >
+      Your Text Here
+    </div>
+  </a>
 
- <div class="carousel-item w-96 h-96 relative block">
+  <div class="carousel-item w-96 h-96">
     <img src="trinity.png" class="rounded-box w-full h-full object-cover" />
-    <div class="text-center mt-2 ml-2 absolute bottom-2 left-0 font-semibold text-white">Your Text Here</div>
-</div>
+  </div>
 
-  
   <div class="carousel-item w-96 h-96">
     <img
       src="mininal_bathroom.jpeg"
